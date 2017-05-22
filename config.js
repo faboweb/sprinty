2c5b49c8516259842027d379310d43b3a44e076a
Fabian Weber <frznhope@gmail.com>
2017-05-22 11:11:14 +0200

    adding config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    gitProxy: 'http://localhost:8989',
+    gitAPIHost: 'api.github.com',
+    gitOAuthUrl: 'https://github.com/login/oauth',
+    gitClientSecret: '',
+    gitClientId: '',
+    googleClientId: '',
+}
