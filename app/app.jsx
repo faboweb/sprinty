7a0ed551b7e942da1abcb44708ca3552f70fcf1e
Fabian Weber <frznhope@gmail.com>
2017-05-23 15:08:04 +0200

    header styling

--- app/app.jsx
+++ app/app.jsx
@@ -18,39 +18,35 @@ router$.$(['route', 'params']).distinct().map(([route, params]) => {
     }
 })
 
-let app = <div class="demo-layout-transparent mdl-layout mdl-js-layout">
+let app = <div class="demo-layout-transparent mdl-layout mdl-js-layout mdl-layout--fixed-header mdl-layout--fixed-tabs">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Sprinty</span>
+            <div class="mdl-layout-spacer"></div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                <input class="mdl-textfield__input" id="org" onchange={e => owner$(e.target.value)} value={owner$}/>
+                <label class="mdl-textfield__label" for="org">Org</label>
+            </div>
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                <input class="mdl-textfield__input" id="org" onchange={e => project$(e.target.value)} value={project$}/>
+                <label class="mdl-textfield__label" for="org">Project</label>
+            </div>
+            {
+                gitOAuthToken$.map(x => x==''
+                    ? <a class="mdl-button mdl-js-button mdl-button--align-right"
+                        href={getGitCodeUrl(config.gitOAuthUrl, config.gitClientId)}>GitHub Login</a>
+                    : <button class="mdl-button mdl-js-button mdl-button--align-right"
+                        onclick={()=>logout(OAUTH_CODE, gitOAuthToken$)}>GitHub Logout</button>)
+            }
+        </div>
+        <div class="mdl-layout__tab-bar">
+            <a class={router$.$('route').map(route => "mdl-layout__tab" + (route === '/' ? ' is-active' : ''))}
+                href="/">Planing</a>
+            <a class={router$.$('route').map(route => "mdl-layout__tab" + (route === '/report' ? ' is-active' : ''))}
+                href="/report">Report</a>
+        </div>
+    </header>
     <main class="mdl-layout__content">
-        <div class="mdl-grid">
-            <div class="mdl-cell mdl-cell--4-col">
-                {
-                    gitOAuthToken$.map(x => x==''
-                        ? <a class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
-                            href={getGitCodeUrl(config.gitOAuthUrl, config.gitClientId)}>GitHub Login</a>
-                        : <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored"
-                            onclick={()=>logout(OAUTH_CODE, gitOAuthToken$)}>GitHub Logout</button>)
-                }
-            </div>
-            <div class="mdl-cell mdl-cell--2-col">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" id="org" onchange={e => owner$(e.target.value)} value={owner$}/>
-                    <label class="mdl-textfield__label" for="org">Org</label>
-                </div>
-            </div>
-            <div class="mdl-cell mdl-cell--2-col">
-                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                    <input class="mdl-textfield__input" id="org" onchange={e => project$(e.target.value)} value={project$}/>
-                    <label class="mdl-textfield__label" for="org">Project</label>
-                </div>
-            </div>
-        </div>
-        <div class="mdl-grid">
-            <div class="mdl-cell mdl-cell--2-col">
-                <a class="mdl-button mdl-js-button" href="/">Planing</a>
-            </div>
-            <div class="mdl-cell mdl-cell--2-col">
-                <a class="mdl-button mdl-js-button" href="/report">Report</a>
-            </div>
-        </div>
         <div class="mdl-grid">
             <Router route='/report' router$={router$}>
                 <SprintProgress gitOAuthToken$={gitOAuthToken$} owner$={owner$} project$={project$} />
--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 export default {
     gitProxy: 'http://localhost:8989',
+    gitOAuthUrl: 'https://github.bus.zalan.do/login/oauth',
+    gitClientId: '0f0a225dd3d8392539c9',
     default_org: 'ais',
     default_repo: 'omega'
 }
