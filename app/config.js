0b0c60d21f7e68f3541590478b68c996ecd4bd2d
Fabian <faboweber@gmail.com>
2017-06-02 19:14:49 +0200

    merge

--- app/config.js
+++ app/config.js
@@ -1,9 +1,5 @@
 export default {
-<<<<<<< HEAD
     gitProxy: '',
-=======
-    gitProxy: 'http://localhost:8989',
->>>>>>> 23efce0... saving default configs
     gitOAuthUrl: 'https://github.com/login/oauth',
     gitClientId: '',
     default_org: 'faboweb',
